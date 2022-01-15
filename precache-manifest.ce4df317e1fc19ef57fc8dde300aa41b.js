1eca76b416a911e544d717d5b9d8fe72f4f0ed8e
mckimssongg <86619053+mckimssongg@users.noreply.github.com>
2022-01-15 12:36:09 -0600

    Updates

---
+++ precache-manifest.ce4df317e1fc19ef57fc8dde300aa41b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24346b9ded99f1aedb465eb588be1164",
+    "url": "/To-do/index.html"
+  },
+  {
+    "revision": "cd6b519f5405bd5483e1",
+    "url": "/To-do/static/css/main.5a17aa6e.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/To-do/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "cd6b519f5405bd5483e1",
+    "url": "/To-do/static/js/main.e919e473.chunk.js"
+  },
+  {
+    "revision": "76256123524e3503b7b6",
+    "url": "/To-do/static/js/runtime~main.00b937a1.js"
+  }
+]);
